5107ea40f99d48329c3b49bd36ddbfcc83ff1de5
coder8080 <67740875+coder8080@users.noreply.github.com>
2023-01-26 08:16:17 +0000

    Урок переименован из-за ошибки на windows

---
+++ PyGame 5. Изображения. Спрайты/1.py
@@ -0,0 +1,7 @@
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('HelloWorld')
+    size = width, height = 500, 500
+    screen = pygame.display.set_mode(size)
